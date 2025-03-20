840d718d0d87f313b3c665a33e809d75c0308403
Kokie <167613469+NittarPP@users.noreply.github.com>
2025-03-20 22:54:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Photon Server"];
+const statusMessages = ["🎧 Listening to Photon Server","😥 Cry to the Heaven","🎧 Listening to Skidder"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
